ca696aafc311c9b47ff5c1baac201942d313bbeb
moshe.frydmann <moshe.frydmann@gmail.com>
2021-09-17 09:55:21 +0300

    display fonctionnel juste en écrivant

--- research.js
+++ research.js
@@ -1,33 +1,70 @@
 let newRecipes = [...recipes];
+let arrayRecipes = [];
 let inputSearch = document.getElementsByClassName("input-research")[0];
+const containerRecipes = document.getElementsByClassName('recipes-container')[0];
+const btnSearch = document.getElementsByClassName('icone-loupe')[0];
 let inputValue = "";
 
-function researchKeyWordl(value) {
+function cleanDisplay() {
 
-    let arrayRecipes = [];
+
+    arrayRecipes.length = 0;
+    
+    while (containerRecipes.firstChild) {
+        containerRecipes.removeChild(containerRecipes.lastChild);
+    }
+};
+
+function displayRecipes() {
+
+    arrayRecipes.map(recipe => {
+
+        const containerRecipe = document.createElement('div');
+        containerRecipe.className = "recipe";
+        containerRecipes.appendChild(containerRecipe);
+
+        const divTitle = document.createElement('div');
+        containerRecipe.textContent = recipe.name;
+        containerRecipe.appendChild(divTitle);
+
+        const divIngredient = document.createElement('div');
+
+        for (let i = 0; i < recipe.ingredients.length; i++) {
+
+            containerRecipe.textContent += ` ${recipe.ingredients[i].ingredient}`;
+        }
+        containerRecipe.appendChild(divIngredient);
+
+
+    })
+
+};
+
+function researchKeyWordl(value) {
 
     for (let i = 0; i < newRecipes.length; i++) {
 
         if (newRecipes[i].description.includes(value)) {
             arrayRecipes.push(newRecipes[i]);
+            continue;
         }
 
-        if (newRecipes[i].name.includes(value)) {
+        else if (newRecipes[i].name.includes(value)) {
             arrayRecipes.push(newRecipes[i]);
+            continue;
         } 
- 
-        for (let j = 0; j < newRecipes[i].ingredients.length; j++) {
 
-            if (newRecipes[i].ingredients[j].ingredient.includes(value)) {
-                arrayRecipes.push(newRecipes[i]);
-                break;
+        else {
+
+            for (let j = 0; j < newRecipes[i].ingredients.length; j++) {
+    
+                if (newRecipes[i].ingredients[j].ingredient.includes(value)) {
+                    arrayRecipes.push(newRecipes[i]);
+                    break;
+                }
             }
         }
     }
-
-    console.log(arrayRecipes)
-
-
 };
 
 inputSearch.addEventListener("input", (e) => {
@@ -35,7 +72,14 @@ inputSearch.addEventListener("input", (e) => {
     inputValue = e.target.value;
 
     if (inputValue.length > 2) {
+        cleanDisplay();    
         researchKeyWordl(inputValue);
+        displayRecipes();
     }
 
 });
+
+/* btnSearch.addEventListener('click', () => {
+    cleanDisplay();    
+}) */
+
