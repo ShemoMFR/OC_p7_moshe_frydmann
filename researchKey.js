abd247b1608ced4f5010293d75374d97d1dc968b
moshe.frydmann <moshe.frydmann@gmail.com>
2021-09-19 11:56:28 +0300

    search Key V2 design

--- researchKey.js
+++ researchKey.js
@@ -6,12 +6,16 @@ function focusResearchKey() {
 
         inputResearchKey[i].addEventListener('focus', () => {
             inputResearchKey[i].value = "";
-            inputResearchKey[i].placeholder = `Rechercher un ${inputResearchKey[i].id}`;
+            inputResearchKey[i].placeholder = `Rechercher dans ${inputResearchKey[i].id}`;
+            inputResearchKey[i].style.width = "300px";
+            document.getElementsByClassName("fleche")[i].style.right = "10%";
         })
 
         inputResearchKey[i].addEventListener("blur", () => {
             inputResearchKey[i].value = inputResearchKey[i].id;
             inputResearchKey[i].placeholder = "";
+            inputResearchKey[i].style.width = "200px";
+            document.getElementsByClassName("fleche")[i].style.right = "20%";
 
         })
     }
