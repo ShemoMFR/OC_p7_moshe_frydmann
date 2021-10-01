ab352efe9b8d6ec7c9374d1bd6186bdc61b56514
moshe.frydmann <moshe.frydmann@gmail.com>
2021-10-01 09:31:14 +0300

    tags added

--- researchKey.js
+++ researchKey.js
@@ -3,6 +3,73 @@ const inputResearchKey = document.querySelectorAll(".search-key");
 const fleche = document.querySelectorAll(".fleche");
 const tags = document.querySelectorAll('.container-tags');
 const mainContainer = document.getElementsByClassName('main-container')[0];
+let unitList = {
+    ingredients: [],
+    ustensiles: [],
+    appareil: []
+};
+
+function createUnitList() {
+
+    let k = 0;
+    let l = 0;
+    let m = 0;
+
+     for (let i = 0; i < recipes.length; i++) {
+
+        if (!(unitList.appareil.includes(recipes[i].appliance))) {
+            unitList.appareil[m] = recipes[i].appliance;
+            m++;
+        }
+
+        for (let j = 0; j< recipes[i].ingredients.length; j++) {
+
+            if (!(unitList.ingredients.includes(recipes[i].ingredients[j].ingredient))) {
+                unitList.ingredients[k] = recipes[i].ingredients[j].ingredient;
+                k++;
+            }
+        }
+
+        for (let j = 0; j< recipes[i].ustensils.length; j++) {
+
+            if (!(unitList.ustensiles.includes(recipes[i].ustensils[j]))) {
+                unitList.ustensiles[l] = recipes[i].ustensils[j];
+                l++;
+            }
+        }
+       
+    } 
+    
+}
+
+function displayTags() {
+
+    tags[0].textContent = `${unitList.ingredients[0]} `;
+    tags[1].textContent = `${unitList.ustensiles[0]} `;
+    tags[2].textContent = `${unitList.appareil[0]} `;
+
+    for (let j = 0; j < tags.length; j++) {
+
+        if (j === 0) {
+            for(let i = 1; i < unitList.ingredients.length; i++) {
+                tags[j].textContent += `${unitList.ingredients[i]} `;
+            }
+        }
+
+        else if (j === 1) {
+            for(let i = 1; i < unitList.ustensiles.length; i++) {
+                tags[j].textContent += `${unitList.ustensiles[i]} `;
+            }
+        }
+
+        else if (j === 2) {
+            for(let i = 1; i < unitList.appareil.length; i++) {
+                tags[j].textContent += `${unitList.appareil[i]} `;
+            }
+        }
+    }
+
+}
 
 function closeResearchArea(i) {
     inputResearchKey[i].placeholder = "";
@@ -57,7 +124,7 @@ function focusResearchKey() {
         inputResearchKey[i].addEventListener('focus', () => {
             inputResearchKey[i].value = "";
             inputResearchKey[i].placeholder = `Rechercher dans ${inputResearchKey[i].id}`;
-            inputResearchKey[i].style.width = "300px";
+            inputResearchKey[i].style.width = "500px";
             fleche[i].style.right = "10%";
         })
 
@@ -80,11 +147,11 @@ function openResearchKey() {
                 isOpen[i] = false;
                 inputResearchKey[i].value = "";
                 inputResearchKey[i].placeholder = `Rechercher dans ${inputResearchKey[i].id}`;
-                inputResearchKey[i].style.width = "300px";
+                inputResearchKey[i].style.width = "500px";
                 inputResearchKey[i].style.borderRadius = "5px 5px 0 0";
                 fleche[i].style.right = "10%";
                 fleche[i].src = "./logo/fleche-haut.svg";
-                tags[i].style.display = "grid";
+                tags[i].style.display = "flex";
             }
 
             else {
@@ -112,6 +179,8 @@ function displayResearchKey() {
 focusResearchKey();
 openResearchKey();
 displayResearchKey();
+createUnitList();
+displayTags();
 
 
 
